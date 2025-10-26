ef46b458a6620c14981f125185e10c1bfe73604b
danukamajaya <danukamajaya@gmail.com>
2025-10-26 19:41:28 +0700

    Adjust column widths for logo and title layout

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ for p in ["logo_kariadi.png", "./logo_kariadi.png", "/app/logo_kariadi.png"]:
         logo = p
         break
 
-col_logo, col_title = st.columns([0.90, 0.75])
+col_logo, col_title = st.columns([0.25, 0.90])
 
 with col_logo:
     if logo:
